72c09c88ee50286d55828bdbd7b64773523781aa
Samuel <samuferfort@gmail.com>
2025-01-23 05:03:39 +0100

    remove already default lazynvim settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 14 -- Keep 12 lines above/below cursor when scrolling
-vim.opt.wrap = false -- Don't wrap long lines
-vim.opt.cursorline = true -- Highlight the current line
-vim.opt.showmatch = true -- Highlight matching brackets
 vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Unless uppercase is used in search
 -- Some nice ones for visual feedback
