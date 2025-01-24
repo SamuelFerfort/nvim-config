a4eef7b54a8f2620a626742ac380aac4c794f00a
Samuel <samuferfort@gmail.com>
2025-01-24 08:33:32 +0100

    add test core and harpoon wich discriptions

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,23 +9,23 @@ return {
     -- mark files and see the menu
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
-    end)
+    end, { desc = "Harpoon: Add file" })
     vim.keymap.set("n", "<C-e>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = "Harpoon: Toggle quick menu" })
 
     -- quick file switching
     vim.keymap.set("n", "<leader>1", function()
       harpoon:list():select(1)
-    end)
+    end, { desc = "Harpoon: Go to file 1" })
     vim.keymap.set("n", "<leader>2", function()
       harpoon:list():select(2)
-    end)
+    end, { desc = "Harpoon: Go to file 2" })
     vim.keymap.set("n", "<leader>3", function()
       harpoon:list():select(3)
-    end)
+    end, { desc = "Harpoon: Go to file 3" })
     vim.keymap.set("n", "<leader>4", function()
       harpoon:list():select(4)
-    end)
+    end, { desc = "Harpoon: Go to file 4" })
   end,
 }
