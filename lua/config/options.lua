1e4ffe0760aee4b2c3903710c308b62c08afa357
Samuel <samuferfort@gmail.com>
2025-01-24 04:23:28 +0100

    clean up options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Options are automatically loaded before lazy.nvim startup
 vim.opt.scrolloff = 14 -- Keep 12 lines above/below cursor when scrolling
 vim.opt.ignorecase = true -- Ignore case when searching
 vim.opt.smartcase = true -- Unless uppercase is used in search
+
+-- Fix for confirmation dialogs
+vim.opt.confirm = true -- Show dialog boxes instead of command-line messages
+local shortmess = vim.o.shortmess:gsub("F", "") -- Remove 'F' from shortmess
+vim.opt.shortmess = shortmess
+
 -- Some nice ones for visual feedback
 vim.opt.updatetime = 100 -- Faster completion
 vim.opt.timeoutlen = 300 -- Time to wait for mapped sequence to complete
