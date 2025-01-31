2aefdc9ec6c603299799e9a7c2dbdbbbc9a1a7b0
Samuel <samuferfort@gmail.com>
2025-01-31 19:35:47 +0100

    improve completions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.keymap.set("n", "<Tab>", ":", { noremap = true })
 vim.keymap.set("n", "<C-a>", "gg0VG", { desc = "Select all" })
--- move lines up and down in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv", { desc = "Move line down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv", { desc = "Move line up" })
+-- move lines up and down in visual mode with better selection preservation
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 -- quick show all problems in a project-wide list
 vim.keymap.set("n", "ç", "<cmd>Trouble<cr>", { desc = "Show trouble/problems" })
 -- home row movement in insert mode
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,11 +2,26 @@ return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
     local cmp = require("cmp")
+
+    -- We're extending the default mappings rather than overwriting them
+    -- This way we keep the good defaults and just add our customizations
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      -- Disable enter key for completion because it's easy to accidentally
+      -- trigger when you just want a new line
       ["<CR>"] = cmp.config.disable,
+
+      -- Use Ctrl-j/k for navigation - this follows vim philosophy of using j/k
+      -- for motion and keeps your hands on the home row
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
+
+      -- Tab to confirm completion - more ergonomic than Enter and
+      -- prevents conflicts with newlines
       ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+
+      -- Explicitly disable up/down to build good navigation habits
+      -- fallback() means these keys will still work normally when completion
+      -- menu is not visible
       ["<Up>"] = cmp.mapping(function(fallback)
         fallback()
       end),
@@ -14,9 +29,20 @@ return {
         fallback()
       end),
     })
+
+    -- Define our completion sources in priority order
+    opts.sources = cmp.config.sources({
+      { name = "nvim_lsp" }, -- LSP completions
+      { name = "buffer" }, -- Text within current buffer
+      { name = "path" }, -- File system paths
+    })
+
+    -- Keep ghost_text on because it works well with Copilot
+    -- and gives good preview of completions
     opts.experimental = {
-      ghost_text = false,
+      ghost_text = true,
     }
+
     return opts
   end,
 }
