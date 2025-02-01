6f53ec8add02765428b88d3d33a0dbaad9c02df8
Samuel <samuferfort@gmail.com>
2025-02-01 13:21:30 +0100

    chore: quick commit 2025-02-01

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,3 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 -- quick show all problems in a project-wide list
 vim.keymap.set("n", "ç", "<cmd>Trouble<cr>", { desc = "Show trouble/problems" })
--- home row movement in insert mode
-vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move cursor left" })
-vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move cursor right" })
--- quick backspace/delete
-vim.keymap.set("i", "<C-u>", "<BS>", { desc = "Backspace" })
-vim.keymap.set("i", "<C-d>", "<Del>", { desc = "Delete" })
