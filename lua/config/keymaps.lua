98de661e682e7711dab9bc137ec91874fd694244
Samux <samuferfort@gmail.com>
2025-02-05 22:29:18 +0100

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,3 @@ vim.keymap.set("n", "<C-a>", "gg0VG", { desc = "Select all" })
 -- move lines up and down in visual mode with better selection preservation
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
--- quick show all problems in a project-wide list
-vim.keymap.set("n", "ç", "<cmd>Trouble<cr>", { desc = "Show trouble/problems" })
